827ffcd517ce0abf63e3fe47d164182c3382d96a
izisopou <54803536+izisopou@users.noreply.github.com>
2022-09-25 11:34:26 +0300

    Update PlotL1L2L3ClosureVsPt.py

--- scripts/PlotL1L2L3ClosureVsPt.py
+++ scripts/PlotL1L2L3ClosureVsPt.py
@@ -11,10 +11,9 @@ TDR.extraText2  = "Preliminary"
 
 class L1L2L3ClosureVsPt():
     def __init__(self, isDefault = False):
-        self.inputPath = "condor_AK4CHS/Files/Run3Winter22_Flat2018QCD/L1L2L3_Closure/test/"
-        self.outputPath = "condor_AK4CHS/Files/Run3Winter22_Flat2018QCD/L1L2L3_Closure/test/"
+        self.inputPath = "Input/path/"
+        self.outputPath = "Output/path/"
         self.fname  = self.inputPath+"ClosureVsRefPt.root"
-	#self.fname  = self.inputPath+"L1L2L3ClosureVsPt_AK8CHS_default.root"
         self.years = [""]
         self.etabins = ["1.3","2.5","3.0","5.0"]
         if isDefault:
@@ -50,48 +49,36 @@ class L1L2L3ClosureVsPt():
                         "color":  ROOT.kGreen+3,
                         "marker": ROOT.kFullCircle,
                         "msize":  1.0,
-                        "hname":  "ak4pfchs/ClosureVsRefPt_JetEta0to1.3",
+                        "hname":  "ak4puppi/ClosureVsRefPt_JetEta0to1.3",
                       },
                 "2.5": {"legend": "1.3 < |#eta| < 2.5",
                         "color":  ROOT.kBlue-4,
                         "marker": ROOT.kFullStar,
                         "msize":  1.3,
-                        "hname":  "ak4pfchs/ClosureVsRefPt_JetEta1.3to2.5",
+                        "hname":  "ak4puppi/ClosureVsRefPt_JetEta1.3to2.5",
                 },
                 "3.0": {"legend": "2.5 < |#eta| < 3.0",
                         "color":  ROOT.kRed+1,
                         "marker": ROOT.kFullTriangleUp,
                         "msize":  1.0,
-                        "hname":  "ak4pfchs/ClosureVsRefPt_JetEta2.5to3",
+                        "hname":  "ak4puppi/ClosureVsRefPt_JetEta2.5to3",
                 },
                 "5.0": {"legend": "3.0 < |#eta| < 5.0",
                         "color":  ROOT.kOrange+1,
                         "marker": ROOT.kFullTriangleDown,
                         "msize":  1.0,
-                        "hname":  "ak4pfchs/ClosureVsRefPt_JetEta3to5",
+                        "hname":  "ak4puppi/ClosureVsRefPt_JetEta3to5",
                 },
             }
 
     def Plot(self):
-        #TDR.cms_lumi_TeV = "QCD multijet "
-	#TDR.cms_lumi_TeV = TDR.commonScheme["legend"][year]+" Legacy"
-	#TDR.cms_lumi_TeV = "Run2 Legacy "
-        #extraText3.append("#bf{anti-k_{T} (R = 0.4)}")
-        #extraText3.append("#bf{PF+PUPPI}")
-        #TDR.extraText3.append("#bf{Anti-k_{T} (R = 0.4), PF+CHS}")
-	#TDR.extraText3.append("AK4, PF+CHS")
         PlotXMin = 11
         PlotXMax = 5500
-        PlotYMin = 0.96
-        PlotYMax = 1.04
+        PlotYMin = 0.97
+        PlotYMax = 1.03
         for year in self.years:
-	    #TDR.cms_lumi_TeV = "QCD multijet "
-	    #TDR.cms_lumi_TeV = TDR.commonScheme["legend"][year]+" Legacy"
-            TDR.cms_lumi_TeV = "Run3Winter22 "
-            #extraText3.append("#bf{anti-k_{T} (R = 0.4)}")
-            #extraText3.append("#bf{PF+PUPPI}")
-            #TDR.extraText3.append("#bf{Anti-k_{T} (R = 0.4), PF+CHS}")
-            TDR.extraText3.append("AK4, PF+CHS")
+            TDR.cms_lumi_TeV = "Winter22Run3 "
+            TDR.extraText3.append("AK4, PF+PUPPI")
 
             #f_ = ROOT.TFile(self.fname.replace("default",year))
             f_ = ROOT.TFile(self.fname)
@@ -120,18 +107,14 @@ class L1L2L3ClosureVsPt():
                 self.Hists[bin]["hist"] = f_.Get(self.Hists[bin]["hname"])
                 self.Hists[bin]["hist"].SetDirectory(0)
                 self.Hists[bin]["hist"].SetMarkerSize(self.Hists[bin]["msize"])
-                #Temp fix: Need to improve closure at high pt due to stat
                 for x in range(1,self.Hists[bin]["hist"].GetNbinsX()+1):
                     if self.Hists[bin]["hist"].GetBinCenter(x)<15 : self.Hists[bin]["hist"].SetBinContent(x,0)
-                    #if self.Hists[bin]["hist"].GetBinError(x)>0.018: self.Hists[bin]["hist"].SetBinContent(x,0)
-		    #if self.Hists[bin]["hist"].GetBinCenter(x)>400 : self.Hists[bin]["hist"].SetBinContent(x,0)
-		    #if self.Hists[bin]["hist"].GetBinCenter(x)>400 and self.Hists[bin]["hist"].GetBinContent(x)>1.01: self.Hists[bin]["hist"].SetBinContent(x,0) 
                 tdrDraw(self.Hists[bin]["hist"], "P", marker=marker, mcolor=color )
                 leg.AddEntry(self.Hists[bin]["hist"], self.Hists[bin]["legend"], "lp")
 
             f_.Close()
-            canv.SaveAs(self.outputPath+"L1L2L3ClosureVsPt_Run3Winter22_AK4CHS.pdf")
-            canv.SaveAs(self.outputPath+"L1L2L3ClosureVsPt_Run3Winter22_AK4CHS.png")
+            canv.SaveAs(self.outputPath+"L1L2L3ClosureVsPt_Winter22Run3_AK4PUPPI.pdf")
+            canv.SaveAs(self.outputPath+"L1L2L3ClosureVsPt_Winter22Run3_AK4PUPPI.png")
             del TDR.extraText3[:]
 
 def main():
