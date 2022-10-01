d38a005712ecf4dd2b4e4e6071183de4725a10d3
izisopou <54803536+izisopou@users.noreply.github.com>
2022-10-01 13:20:08 +0300

    Update Setup_FileLocation.sh

--- condor_AK4CHS/Setup_FileLocation.sh
+++ condor_AK4CHS/Setup_FileLocation.sh
@@ -8,8 +8,8 @@ WithPUFiles=/eos/cms/store/group/phys_jetmet/ilias/Run3MCtruthSamples/NoPtCut/AK
 #WithPUFiles=/eos/cms/store/group/phys_jetmet/ilias/Run3MCtruthSamples/NoPtCut/AK4/Winter22/PremixedPU/QCD_Pt-15to7000_TuneCP5_Flat2018_13p6TeV_pythia8/Run3Winter22_PremixedPU/220902_080010/0000/*.root
 
 # Temporary folders (change to your eos path)
-Step1Output=/eos/cms/store/group/phys_jetmet/ilias/Step1/
-Step2Output=/eos/cms/store/group/phys_jetmet/ilias/Step2/
-Step3Output=/eos/cms/store/group/phys_jetmet/ilias/Step3/
-Step4Output=/eos/cms/store/group/phys_jetmet/ilias/Step4/
+Step1Output=/eos/cms/store/group/phys_jetmet/ilias/Step1_AK4CHS/
+Step2Output=/eos/cms/store/group/phys_jetmet/ilias/Step2_AK4CHS/
+Step3Output=/eos/cms/store/group/phys_jetmet/ilias/Step3_AK4CHS/
+Step4Output=/eos/cms/store/group/phys_jetmet/ilias/Step4_AK4CHS/
 
