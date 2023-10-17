4c9d8c65f5d648fc9597a780dd2172b1051f1c8e
izisopou <54803536+izisopou@users.noreply.github.com>
2023-10-17 10:50:31 +0300

    Update jet_response_analyzer_x.cc

--- JetAnalyzers/bin/jet_response_analyzer_x.cc
+++ JetAnalyzers/bin/jet_response_analyzer_x.cc
@@ -196,6 +196,7 @@ int main(int argc,char**argv)
   TString        DataPUHistoName   = cl.getValue<TString>("DataPUHistoName","pileup_jt400");
   bool           doDZcut           = cl.getValue<bool>   ("doDZcut",                 false);
   bool           doNMcut           = cl.getValue<bool>   ("doNMcut",                 false);
+  bool           doVetoMap_EE      = cl.getValue<bool>   ("doVetoMap_EE",            false);
   bool           verbose           = cl.getValue<bool>   ("verbose",                 false);
 
   if (!cl.check()) return 0;
@@ -1439,6 +1440,11 @@ int main(int argc,char**argv)
     }
     
 
+    TFile *f_veto = new TFile("/afs/cern.ch/work/i/izisopou/public/JEC_NewMethods/CMSSW_13_0_3/src/JetMETAnalysisMCtruth/MyDataMCHistos/VetoMaps_Winter22Run3_RunE_v1.root","READ");
+
+    TH2D *h_veto = (TH2D*)f_veto->Get("jetvetomap_all");
+
+
     //
     // fill histograms
     //
@@ -1490,11 +1496,23 @@ int main(int argc,char**argv)
         if (nrefmax>0) JRAEvt->nref = std::min((int)JRAEvt->nref,nrefmax);
         for (unsigned char iref=0;iref<JRAEvt->nref;iref++) {
 	        
-         //=== veto region for UL2017 =======
-        //if((JRAEvt->jtphi->at(iref)<-0.5236 && JRAEvt->jtphi->at(iref)>-0.8727 && JRAEvt->jteta->at(iref) >1.31 && JRAEvt->jteta->at(iref)<2.96) || (JRAEvt->jtphi->at(iref)>2.705 && JRAEvt->jtphi->at(iref)<3.1416 && JRAEvt->jteta->at(iref) >0 && JRAEvt->jteta->at(iref)<1.4835) )continue;
-        //=== veto region for UL2018 =======
-        //if((JRAEvt->jtphi->at(iref)<-0.8727 && JRAEvt->jtphi->at(iref)>-1.5708 && JRAEvt->jteta->at(iref) < -1.31 && JRAEvt->jteta->at(iref)> -2.96) || (JRAEvt->jtphi->at(iref)>0.4363 && JRAEvt->jtphi->at(iref)<0.7854 && JRAEvt->jteta->at(iref) >0 && JRAEvt->jteta->at(iref)<1.31) )continue;
-	  
+
+	  if(doVetoMap_EE){
+		bool flag_IsInVetoRegion = false;
+
+		for(int ibin=1; ibin<=h_veto->GetNbinsX(); ibin++){
+			for(int jbin=1; jbin<=h_veto->GetNbinsY(); jbin++){
+				if(h_veto->GetBinContent(ibin, jbin)!=0){
+					if( (h_veto->GetXaxis()->GetBinLowEdge(ibin) < JRAEvt->jteta->at(iref)) && (JRAEvt->jteta->at(iref) < h_veto->GetXaxis()->GetBinLowEdge(ibin+1)) && (h_veto->GetYaxis()->GetBinLowEdge(jbin) < JRAEvt->jtphi->at(iref)) && (JRAEvt->jtphi->at(iref) < h_veto->GetYaxis()->GetBinLowEdge(jbin+1))) flag_IsInVetoRegion = true;
+				}
+			}
+		}
+
+		if(flag_IsInVetoRegion==true) continue;
+
+	  }
+
+	   
 
 	  //Apply Neutral Multiplicity cut for |eta|>3 to remove the double peak in PUPPI
 	  if(doNMcut){
